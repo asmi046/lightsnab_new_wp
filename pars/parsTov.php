3667361049ac45e12fad0d01dd48b1bcf17e472e
asmi046 <asmi046@gmail.com>
2020-11-24 23:38:06 +0300

    Создание поста

--- pars/parsTov.php
+++ pars/parsTov.php
@@ -9,6 +9,7 @@
         
         $tovarInfo["name"] = $html->find('h1')[0]->plaintext;
         $tovarInfo["sku"] = str_replace(array(" ","Артикул:"), "", $html->find('.product-summary')[0]->plaintext);
+        $tovarInfo["price"] = $html->find('.price')[0]->attr["data-real-price"];
         $descr = $html->find('.product-description')[0]->innertext;
         $tovarInfo["description"] = substr( $descr, 0, strpos( $descr, "<table>"));
         $tovarInfo["caracter"] = array();
@@ -23,6 +24,64 @@
             $tovarInfo["caracter"][] =  $toc;
         }
 
+        $tovarInfo["modification"] = array();
+
+        foreach($html->find('.select-skus option') as $cr) {
+            $opt["name"] = $cr->plaintext;
+            $opt["sku"] = $cr->value;
+            $opt["price"] = $cr->attr["data-price"];
+            $opt["price_old"] = $cr->attr["data-compare-price"];
+            $tovarInfo["modification"][] =  $opt;
+        }
+
+        $tovarInfo["images"] = array();
+
+        foreach($html->find('.main-gallery img') as $cr) {
+            $img["lnk"] = "https://efimlight.ru".$cr->src;
+           
+            $tovarInfo["images"][] =  $img;
+        }
+
+        if (!file_exists("tovars")) mkdir("tovars");
+        if (!file_exists("tovars/".$tovarInfo["name"])) mkdir("tovars/".$tovarInfo["name"]);
+
+        $pindex = 0;
+        foreach($tovarInfo["images"] as $pict) {
+            $filename = $tovarInfo["name"]."-".$tovarInfo["sku"]."-".$pindex.".".pathinfo($pict["lnk"], PATHINFO_EXTENSION);
+            $dirname = "tovars/".$tovarInfo["name"];
+            copy($pict["lnk"],   $dirname."/".$filename);
+            
+            $tovarInfo["images"][$pindex]["lnk"] = "/pars/".$dirname."/".$filename;   
+            $pindex++;
+        }
+        file_put_contents($dirname."/content-".$tovarInfo["name"].".json", json_encode($tovarInfo));
+
+        // --------------- Делаем пост --------------------
+        $to_post_meta  = [ 
+            '_offer_smile_descr' => $tovarInfo["description"], 
+            '_offer_type' => "Цокольный (Со сменными лампами)", 
+            '_offer_sku' => $tovarInfo["sku"], 
+            '_offer_nal' => 'В наличии', 
+            '_offer_price' => $tovarInfo["price"]
+        ];
+
+        $indexCh = 0;
+        foreach ($tovarInfo["caracter"] as $carrecter) {
+            $to_post_meta["_offer_cherecter|c_name|".$indexCh."|0|value"] = $carrecter["name"];
+            $to_post_meta["_offer_cherecter|c_val|".$indexCh."|0|value"] = $carrecter["value"];
+            $indexCh++;
+        }
+
+        $post_id = wp_insert_post(  wp_slash( array(
+            'post_type'     => 'light',
+            'post_status'    => 'publish',
+            'post_title' => $tovarInfo["name"],
+            'post_excerpt'  => $tovarInfo["description"],
+            'post_content'  => $tovarInfo["description"],
+            'meta_input'     => $to_post_meta
+        ) ) );
+
+        echo "Пост: ".$post_id."\n\r";
         print_r( $tovarInfo);
         echo "\n\r";
 
---
+++ single-light.php
@@ -0,0 +1,16 @@
+<?php 
+
+/*
+Template Name: Шаблон карточки товара (Default)
+
+*/
+
+get_header(); ?>
+ 
+	<?php get_template_part('template-parts/block-menu');?>
+
+	<?php get_template_part('template-parts/header-category');?>
+
+	<?php get_template_part('template-parts/single-product-section');?>
+
+<?php get_footer(); ?>
