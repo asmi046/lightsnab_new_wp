50ce4b009e3198cb486ea150b4d68e937872331f
asmi046 <asmi046@gmail.com>
2021-05-17 20:58:33 +0300

    Страницы для парсинга мебели

---
+++ pars/pages-mebel.php
@@ -0,0 +1,114 @@
+<?
+    $parsePages = array(
+        array( 
+            "cat" => "Комоды"
+            "pages" => array(
+                "https://loft-concept.ru/catalog/commode/?PAGEN_4=1",
+                "https://loft-concept.ru/catalog/commode/?PAGEN_4=2",
+                "https://loft-concept.ru/catalog/commode/?PAGEN_4=3",
+                "https://loft-concept.ru/catalog/commode/?PAGEN_4=4",
+                "https://loft-concept.ru/catalog/commode/?PAGEN_4=5",
+                "https://loft-concept.ru/catalog/commode/?PAGEN_4=6",
+                "https://loft-concept.ru/catalog/commode/?PAGEN_4=7",
+                "https://loft-concept.ru/catalog/commode/?PAGEN_4=8",
+                "https://loft-concept.ru/catalog/commode/?PAGEN_4=9",
+                "https://loft-concept.ru/catalog/commode/?PAGEN_4=10",
+                "https://loft-concept.ru/catalog/commode/?PAGEN_4=11",
+                "https://loft-concept.ru/catalog/commode/?PAGEN_4=12",
+                "https://loft-concept.ru/catalog/commode/?PAGEN_4=13",
+                "https://loft-concept.ru/catalog/commode/?PAGEN_4=14",
+                "https://loft-concept.ru/catalog/commode/?PAGEN_4=15",
+                "https://loft-concept.ru/catalog/commode/?PAGEN_4=16",
+                "https://loft-concept.ru/catalog/commode/?PAGEN_4=17",
+                "https://loft-concept.ru/catalog/commode/?PAGEN_4=18"
+            )
+        ),
+
+        array( 
+            "cat" => "Диваны"
+            "pages" => array(
+                "https://loft-concept.ru/catalog/sofa/?PAGEN_4=1",
+                "https://loft-concept.ru/catalog/sofa/?PAGEN_4=2",
+                "https://loft-concept.ru/catalog/sofa/?PAGEN_4=3",
+                "https://loft-concept.ru/catalog/sofa/?PAGEN_4=4",
+                "https://loft-concept.ru/catalog/sofa/?PAGEN_4=5",
+                "https://loft-concept.ru/catalog/sofa/?PAGEN_4=6",
+                "https://loft-concept.ru/catalog/sofa/?PAGEN_4=7",
+                "https://loft-concept.ru/catalog/sofa/?PAGEN_4=8",
+                "https://loft-concept.ru/catalog/sofa/?PAGEN_4=9",
+                "https://loft-concept.ru/catalog/sofa/?PAGEN_4=10",
+                "https://loft-concept.ru/catalog/sofa/?PAGEN_4=11",
+                "https://loft-concept.ru/catalog/sofa/?PAGEN_4=12",
+                "https://loft-concept.ru/catalog/sofa/?PAGEN_4=13",
+                "https://loft-concept.ru/catalog/sofa/?PAGEN_4=14",
+                "https://loft-concept.ru/catalog/sofa/?PAGEN_4=15",
+                "https://loft-concept.ru/catalog/sofa/?PAGEN_4=16",
+                "https://loft-concept.ru/catalog/sofa/?PAGEN_4=17",
+                "https://loft-concept.ru/catalog/sofa/?PAGEN_4=18",
+                "https://loft-concept.ru/catalog/sofa/?PAGEN_4=19",
+                "https://loft-concept.ru/catalog/sofa/?PAGEN_4=20",
+                "https://loft-concept.ru/catalog/sofa/?PAGEN_4=21",
+                "https://loft-concept.ru/catalog/sofa/?PAGEN_4=22",
+                "https://loft-concept.ru/catalog/sofa/?PAGEN_4=23",
+                "https://loft-concept.ru/catalog/sofa/?PAGEN_4=24"
+                
+            )
+        ),
+
+        array( 
+            "cat" => "Кресла"
+            "pages" => array(
+                "https://loft-concept.ru/catalog/chair/?PAGEN_4=1",
+                "https://loft-concept.ru/catalog/chair/?PAGEN_4=2",
+                "https://loft-concept.ru/catalog/chair/?PAGEN_4=3",
+                "https://loft-concept.ru/catalog/chair/?PAGEN_4=4",
+                "https://loft-concept.ru/catalog/chair/?PAGEN_4=5",
+                "https://loft-concept.ru/catalog/chair/?PAGEN_4=6",
+                "https://loft-concept.ru/catalog/chair/?PAGEN_4=7",
+                "https://loft-concept.ru/catalog/chair/?PAGEN_4=8",
+                "https://loft-concept.ru/catalog/chair/?PAGEN_4=9",
+                "https://loft-concept.ru/catalog/chair/?PAGEN_4=10",
+                "https://loft-concept.ru/catalog/chair/?PAGEN_4=11",
+                "https://loft-concept.ru/catalog/chair/?PAGEN_4=12",
+                "https://loft-concept.ru/catalog/chair/?PAGEN_4=13",
+                "https://loft-concept.ru/catalog/chair/?PAGEN_4=14",
+                "https://loft-concept.ru/catalog/chair/?PAGEN_4=15",
+                "https://loft-concept.ru/catalog/chair/?PAGEN_4=16",
+                "https://loft-concept.ru/catalog/chair/?PAGEN_4=17",
+                "https://loft-concept.ru/catalog/chair/?PAGEN_4=18",
+                "https://loft-concept.ru/catalog/chair/?PAGEN_4=19",
+                "https://loft-concept.ru/catalog/chair/?PAGEN_4=20",
+                "https://loft-concept.ru/catalog/chair/?PAGEN_4=21",
+                "https://loft-concept.ru/catalog/chair/?PAGEN_4=22",
+                "https://loft-concept.ru/catalog/chair/?PAGEN_4=23",
+                "https://loft-concept.ru/catalog/chair/?PAGEN_4=24",
+                "https://loft-concept.ru/catalog/chair/?PAGEN_4=25",
+                "https://loft-concept.ru/catalog/chair/?PAGEN_4=26",
+                "https://loft-concept.ru/catalog/chair/?PAGEN_4=27",
+                "https://loft-concept.ru/catalog/chair/?PAGEN_4=28",
+                "https://loft-concept.ru/catalog/chair/?PAGEN_4=29",
+                "https://loft-concept.ru/catalog/chair/?PAGEN_4=30",
+                "https://loft-concept.ru/catalog/chair/?PAGEN_4=31",
+                "https://loft-concept.ru/catalog/chair/?PAGEN_4=32",
+                "https://loft-concept.ru/catalog/chair/?PAGEN_4=33",
+                "https://loft-concept.ru/catalog/chair/?PAGEN_4=34",
+                "https://loft-concept.ru/catalog/chair/?PAGEN_4=35",
+                "https://loft-concept.ru/catalog/chair/?PAGEN_4=36",
+                "https://loft-concept.ru/catalog/chair/?PAGEN_4=37",
+                "https://loft-concept.ru/catalog/chair/?PAGEN_4=38",
+                "https://loft-concept.ru/catalog/chair/?PAGEN_4=39",
+                "https://loft-concept.ru/catalog/chair/?PAGEN_4=40",
+                "https://loft-concept.ru/catalog/chair/?PAGEN_4=41",
+                "https://loft-concept.ru/catalog/chair/?PAGEN_4=42",
+                "https://loft-concept.ru/catalog/chair/?PAGEN_4=43",
+                "https://loft-concept.ru/catalog/chair/?PAGEN_4=44",
+                "https://loft-concept.ru/catalog/chair/?PAGEN_4=45",
+                "https://loft-concept.ru/catalog/chair/?PAGEN_4=46",
+                "https://loft-concept.ru/catalog/chair/?PAGEN_4=47",
+                "https://loft-concept.ru/catalog/chair/?PAGEN_4=48"
+
+                
+            )
+        )
+    );
+?>
