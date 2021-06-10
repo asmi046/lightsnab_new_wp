619bc65a966f28498b1c64dcfda4edfd802f10b0
asmi046 <asmi046@gmail.com>
2021-06-10 18:03:33 +0300

    Парсинг мебели 2

--- pars/alllist-mebel.php
+++ pars/alllist-mebel.php
@@ -5,7 +5,7 @@
     require_once("../../../../wp-config.php");
     include_once("simple_html_dom.php");
 
-    include_once("pages-mebel.php");
+    include_once("pages-mebel2.php");
 
     global $wpdb;
 
@@ -20,22 +20,41 @@
         wp_update_term( $termSiteParent->term_id, 'lightcat');
     }
 
+    
+    
+  
     for ($j = 0; $j<count($parsePages); $j++)
     {
         for ($i = 0; $i<count($parsePages[$j]["pages"]); $i++)
         {
-            $html = file_get_html($parsePages[$j]["pages"][$i]);
+            $context = stream_context_create(
+                array(
+                    "http" => array(
+                        "header" => "User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36"
+                    )
+                )
+            );
+            
+          //  $fstr = file_get_contents($parsePages[$j]["pages"][$i], false, $context);
 
-            foreach($html->find('.itembg') as $element)
+            $html = file_get_html($parsePages[$j]["pages"][$i], false, $context);
+
+            foreach($html->find('.product') as $element)
             {
                 $tov = array();    
         
 
-                $tov["name"] = $element->find('.item-name')[0]->plaintext;
+                $tov["name"] = $element->find('.title')[0]->plaintext;
+                
+                if (empty($tov["name"])) continue;
+
                 $tov["cat"] = $parsePages[$j]["cat"];
-                $tov["price"] = trim(str_replace(array(" ","руб."), "", $element->find('.price')[0]->plaintext));
-                $tov["lnk"] = "https://loft-concept.ru".$element->find('a')[0]->href;
-                $tov["main_img"] = "https://loft-concept.ru".$element->find('img')[0]->src;
+                $rub_price = trim(str_replace(array(" ","USD","руб.","₽","$",",",",00",".00"), "", $element->find('.price .money')[0]->plaintext));
+                $rub_price = (float)$rub_price*72;
+
+                $tov["price"] = $rub_price;
+                $tov["lnk"] = "https://louvrehome.ru".$element->find('a')[0]->href;
+                $tov["main_img"] = $element->find('img')[0]->src;
                 $tov["parsed_page"] = $parsePages[$j]["pages"][$i];
                 
                 
@@ -46,6 +65,7 @@
                 
             }
             
+         
         }
 
         $termSite = get_term_by('name', $parsePages[$j]["cat"], 'lightcat');
