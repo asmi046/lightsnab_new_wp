b7b62e3a58837f369b3baab4142e11f5f384176e
Ruvick <rudikov.web@ya.ru>
2021-08-12 00:02:46 +0300

    Добавил в футер иконки соц сетей

--- footer.php
+++ footer.php
@@ -1,42 +1,48 @@
-
-	<footer class="footer">
-    <div class="container">
-      <div class="footer-block">
-        <ul class="ul-clean">
-          <li><a href="<?echo get_category_link(11); ?>">Люстры и дизайнерские светильники</a></li>
-          <li><a href="<?echo get_category_link(53); ?>">Скандинавские люстры</a></li>
-          <li><a href="<?echo get_category_link(16); ?>">Бра и настенное освещение</a></li>
-          <li><a href="<?echo get_category_link(17); ?>">Торшеры</a></li>
-          <li><a href="<?echo get_category_link(18); ?>">Настольные лампы</a></li>
-          <li><a href="<?echo get_category_link(12); ?>">Светильники и подвесной свет</a></li>
-        </ul>
-      </div>
-      <div class="footer-block">
-        <ul class="ul-clean">
-          <li><a href="<?echo get_the_permalink(447)?>">Оптовым покупателям</a></li>
-          <li><a href="<?echo get_the_permalink(393)?>">Доставка и оплата </a></li>
-          <li><a href="<?echo get_the_permalink(396)?>">Возврат и обмен товара </a></li>
-          <li><a href="<?echo get_the_permalink(452)?>">Политика конфиденциальности</a></li>
-        </ul>
-      </div>
-      <div class="footer-block footer-block-last">
-        <div class="">© Lightsnab.ru, 2014—<?echo date("Y")?> </div>
-        <div class="">Телефон: <?echo SITE_PHONE;?></div>
-        <div class="">Телефон: <?echo SITE_PHONE_DOP;?></div>
-        <div class="">E-mail: <?echo SITE_MAIL;?></div>
-        <div class="soc-block">
-          <a class = "soc_btn_insta" href="<?echo INSTA_LNK;?>"></a>
-          <a class = "soc_btn_fb" href="<?echo FB_LNK;?>" ></a>
-        </div>
-        <!-- <a class = "workerlnk" href = "https://asmi-studio.ru/">Разработка сайта <span>Asmi-Studio</span></a> -->
-      </div>
+<footer class="footer">
+  <div class="container">
+    <div class="footer-block">
+      <ul class="ul-clean">
+        <li><a href="<? echo get_category_link(11); ?>">Люстры и дизайнерские светильники</a></li>
+        <li><a href="<? echo get_category_link(53); ?>">Скандинавские люстры</a></li>
+        <li><a href="<? echo get_category_link(16); ?>">Бра и настенное освещение</a></li>
+        <li><a href="<? echo get_category_link(17); ?>">Торшеры</a></li>
+        <li><a href="<? echo get_category_link(18); ?>">Настольные лампы</a></li>
+        <li><a href="<? echo get_category_link(12); ?>">Светильники и подвесной свет</a></li>
+      </ul>
     </div>
-	</footer>
+    <div class="footer-block">
+      <ul class="ul-clean">
+        <li><a href="<? echo get_the_permalink(447) ?>">Оптовым покупателям</a></li>
+        <li><a href="<? echo get_the_permalink(393) ?>">Доставка и оплата </a></li>
+        <li><a href="<? echo get_the_permalink(396) ?>">Возврат и обмен товара </a></li>
+        <li><a href="<? echo get_the_permalink(452) ?>">Политика конфиденциальности</a></li>
+      </ul>
+    </div>
+    <div class="footer-block footer-block-last">
+      <div class="">© Lightsnab.ru, 2014—<? echo date("Y") ?> </div>
+      <div class="">Телефон: <? echo SITE_PHONE; ?></div>
+      <div class="">Телефон: <? echo SITE_PHONE_DOP; ?></div>
+      <div class="">E-mail: <? echo SITE_MAIL; ?></div>
+      <div class="soc-block">
+        <a class="soc_btn_insta" href="<? echo INSTA_LNK; ?>"></a>
+        <a class="soc_btn_fb" href="<? echo FB_LNK; ?>"></a>
+        <a class="soc_btn_telegram" href="<?php echo carbon_get_theme_option('as_telegr'); ?>"></a>
+        <a class="soc_btn_facebook" href="<?php echo carbon_get_theme_option('as_face'); ?>"></a>
+      </div>
+      <div class="soc-block soc-block-bottom">
+        <a class="soc_btn_twitter" href="<?php echo carbon_get_theme_option('as_twiter'); ?>"></a>
+        <a class="soc_btn_ok" href="<?php echo carbon_get_theme_option('as_ok'); ?>"></a>
+        <a class="soc_btn_vk" href="<?php echo carbon_get_theme_option('as_vk'); ?>"></a>
+      </div>
+      <!-- <a class = "workerlnk" href = "https://asmi-studio.ru/">Разработка сайта <span>Asmi-Studio</span></a> -->
+    </div>
+  </div>
+</footer>
 <!-- </div> -->
 
-  <div class="top-btn"></div>
+<div class="top-btn"></div>
 
-  <?php wp_footer(); ?>
+<?php wp_footer(); ?>
 
 </body>
 
--- themes-fields.php
+++ themes-fields.php
@@ -11,150 +11,151 @@
 use Carbon_Fields\Container;
 use Carbon_Fields\Field;
 
-Container::make( 'theme_options', __( 'Настройки темы', 'crb' ) )
-		->add_fields( array(
-			Field::make( 'text', 'mail_to_send', 'E-mail для отправки' )->set_width(50),
-			Field::make( 'checkbox', 'load_rew', 'Принимать отзывы' )->set_option_value('no')->set_width(50),
-			Field::make('rich_text', 'main_fulltext_top', 'Текст для главной страницы (Верхний)')->set_width(100),
-			Field::make('rich_text', 'main_fulltext', 'Текст для главной страницы (SEO)')->set_width(100),
-			Field::make('rich_text', 'obmen_fulltext', 'Текст в раздел обмен возврат')->set_width(100),
+Container::make('theme_options', __('Настройки темы', 'crb'))
+	->add_fields(array(
+		Field::make('text', 'mail_to_send', 'E-mail для отправки')->set_width(50),
+		Field::make('checkbox', 'load_rew', 'Принимать отзывы')->set_option_value('no')->set_width(50),
+		Field::make('rich_text', 'main_fulltext_top', 'Текст для главной страницы (Верхний)')->set_width(100),
+		Field::make('rich_text', 'main_fulltext', 'Текст для главной страницы (SEO)')->set_width(100),
+		Field::make('rich_text', 'obmen_fulltext', 'Текст в раздел обмен возврат')->set_width(100),
+	))
+	->add_tab('Акции', array(
+		Field::make('complex', 'promo_complex', 'Акции')
+			// ->set_max(3) // Можно будет выбрать только 5 постов
+			->add_fields(array(
+				Field::make('image', 'promo_img', 'Фото')
+					->set_width(30),
+				Field::make('text', 'promo_text', 'Текст')
+					->set_width(30),
+				// Field::make('text', 'link_benefit', 'Ссылка')   
+				// 	->set_width(30),
 			))
-		->add_tab('Акции', array(
-		  Field::make('complex', 'promo_complex', 'Акции')
-  			// ->set_max(3) // Можно будет выбрать только 5 постов
-  	->add_fields(array(
-   		Field::make('image', 'promo_img', 'Фото') 
-    		->set_width(30),
-    	Field::make('text', 'promo_text', 'Текст')   
-    		->set_width(30),
-    	// Field::make('text', 'link_benefit', 'Ссылка')   
-    	// 	->set_width(30),
-   	 )) 
-		))
-		->add_tab('Отзывы', array(
-		  Field::make('complex', 'reviews_complex', 'Отзывы')
-  			// ->set_max(3) // Можно будет выбрать только 5 постов
-  	->add_fields(array(
-   		Field::make('image', 'reviews_img', 'Фото')
-    		->set_width(30),
-			Field::make('text', 'reviews_fio', 'ФИО')   
-    		->set_width(30),
-			Field::make('text', 'reviews_date', 'Дата')   
-    		->set_width(30),
-    	Field::make('text', 'reviews_text', 'Отзыв')   
-    		->set_width(30),
-    	// Field::make('text', 'link_benefit', 'Ссылка')   
-    	// 	->set_width(30),
-   	 )) 
-		))
-		->add_tab('Контакты', array(
-			Field::make( 'text', 'as_company', __( 'Название' ) )
-				->set_width(50),
-			Field::make( 'text', 'as_schedule', __( 'Режим работы' ) ) 
-				->set_width(50),
-			Field::make( 'text', 'as_phones_1', __( 'Телефон' ) )
-				->set_width(50),
-			Field::make( 'text', 'as_phone_2', __( 'Телефон дополнительный' ) )
-				->set_width(50),
-			Field::make( 'text', 'as_email', __( 'Email' ) )
-				->set_width(50),
-			// Field::make( 'text', 'as_email_send', __( 'Email для отправки' ) ) 
-			// 	->set_width(50),
-			Field::make( 'text', 'as_inn', __( 'ИНН' ) )
-				->set_width(50),
-			Field::make( 'text', 'as_orgn', __( 'ОРГН' ) )
-				->set_width(50),
-			Field::make( 'text', 'as_kpp', __( 'КПП' ) )
-				->set_width(50),
-			Field::make( 'text', 'as_address', __( 'Адрес' ) )
-				->set_width(50),
-			Field::make( 'text', 'as_bik', __( 'БИК' ) )
-				->set_width(50),
-			Field::make( 'text', 'as_rs', __( 'Р/С' ) )
-				->set_width(50),
-			Field::make( 'text', 'as_ks', __( 'К/С' ) )
-				->set_width(50),
-			Field::make( 'text', 'as_bank', __( 'БАНК' ) )
-				->set_width(50),
-			Field::make( 'text', 'as_insta', __( 'instagram' ) )
-				->set_width(50),
-			Field::make( 'text', 'as_face', __( 'facebook' ) )
-				->set_width(50),
-			Field::make( 'text', 'as_vk', __( 'Вконтакте' ) )
-				->set_width(50),
-			Field::make( 'text', 'as_telegr', __( 'telegram' ) )
-				->set_width(50),
-			Field::make( 'text', 'as_whatsapp', __( 'whatsapp' ) )
-				->set_width(50),
-			Field::make('text', 'map_point', 'Координаты карты')
-				->set_width(50),
-			Field::make('text', 'text_map', 'Текст метки карты')
-				->set_width(50),
-	) );
-		
+	))
+	->add_tab('Отзывы', array(
+		Field::make('complex', 'reviews_complex', 'Отзывы')
+			// ->set_max(3) // Можно будет выбрать только 5 постов
+			->add_fields(array(
+				Field::make('image', 'reviews_img', 'Фото')
+					->set_width(30),
+				Field::make('text', 'reviews_fio', 'ФИО')
+					->set_width(30),
+				Field::make('text', 'reviews_date', 'Дата')
+					->set_width(30),
+				Field::make('text', 'reviews_text', 'Отзыв')
+					->set_width(30),
+				// Field::make('text', 'link_benefit', 'Ссылка')   
+				// 	->set_width(30),
+			))
+	))
+	->add_tab('Контакты', array(
+		Field::make('text', 'as_company', __('Название'))
+			->set_width(50),
+		Field::make('text', 'as_schedule', __('Режим работы'))
+			->set_width(50),
+		Field::make('text', 'as_phones_1', __('Телефон'))
+			->set_width(50),
+		Field::make('text', 'as_phone_2', __('Телефон дополнительный'))
+			->set_width(50),
+		Field::make('text', 'as_email', __('Email'))
+			->set_width(50),
+		// Field::make( 'text', 'as_email_send', __( 'Email для отправки' ) ) 
+		// 	->set_width(50),
+		Field::make('text', 'as_inn', __('ИНН'))
+			->set_width(50),
+		Field::make('text', 'as_orgn', __('ОРГН'))
+			->set_width(50),
+		Field::make('text', 'as_kpp', __('КПП'))
+			->set_width(50),
+		Field::make('text', 'as_address', __('Адрес'))
+			->set_width(50),
+		Field::make('text', 'as_bik', __('БИК'))
+			->set_width(50),
+		Field::make('text', 'as_rs', __('Р/С'))
+			->set_width(50),
+		Field::make('text', 'as_ks', __('К/С'))
+			->set_width(50),
+		Field::make('text', 'as_bank', __('БАНК'))
+			->set_width(50),
+		Field::make('text', 'as_insta', __('instagram'))
+			->set_width(50),
+		Field::make('text', 'as_face', __('facebook'))
+			->set_width(50),
+		Field::make('text', 'as_vk', __('Вконтакте'))
+			->set_width(50),
+		Field::make('text', 'as_twiter', __('twiter'))
+			->set_width(50),
+		Field::make('text', 'as_ok', __('Одноклассники'))
+			->set_width(50),
+		Field::make('text', 'as_telegr', __('telegram'))
+			->set_width(50),
+		Field::make('text', 'as_whatsapp', __('whatsapp'))
+			->set_width(50),
+		Field::make('text', 'map_point', 'Координаты карты')
+			->set_width(50),
+		Field::make('text', 'text_map', 'Текст метки карты')
+			->set_width(50),
+	));
+
 Container::make('post_meta', 'light_product_cr', 'Характеристики товара')
-		->show_on_post_type(array( 'light'))
-		  ->add_fields(array(   
-			Field::make('textarea', 'offer_smile_descr', 'Краткое описание')->set_width(100),
-			Field::make('text', 'offer_name', 'Название товара')->set_width(30),
-			Field::make('text', 'offer_label', 'Метка на товаре')->set_width(30),
-			Field::make('text', 'offer_allsearch', 'Все артикулы для поиска')->set_width(100),
-			Field::make('text', 'offer_siries', 'Серия (для сопутствующих)')->set_width(30),
-			Field::make('select', 'offer_type', 'Тип светильника')->add_options( array(
-				'0' => 'Не задано',
-				'Светодиодный (LED)' => 'Светодиодный (LED)',
-				'Цокольный (Со сменными лампами)' => 'Цокольный (Со сменными лампами)'
-			) )->set_width(100),
+	->show_on_post_type(array('light'))
+	->add_fields(array(
+		Field::make('textarea', 'offer_smile_descr', 'Краткое описание')->set_width(100),
+		Field::make('text', 'offer_name', 'Название товара')->set_width(30),
+		Field::make('text', 'offer_label', 'Метка на товаре')->set_width(30),
+		Field::make('text', 'offer_allsearch', 'Все артикулы для поиска')->set_width(100),
+		Field::make('text', 'offer_siries', 'Серия (для сопутствующих)')->set_width(30),
+		Field::make('select', 'offer_type', 'Тип светильника')->add_options(array(
+			'0' => 'Не задано',
+			'Светодиодный (LED)' => 'Светодиодный (LED)',
+			'Цокольный (Со сменными лампами)' => 'Цокольный (Со сменными лампами)'
+		))->set_width(100),
 
-			 Field::make('text', 'offer_sku', 'Артикул (Базовый)')->set_width(50),
-			Field::make('text', 'offer_nal', 'Наличие на складе')->set_default_value( 'В наличии')->set_width(50),
-			
-			Field::make( 'complex', 'offer_cherecter', "Характеристики товара" )
-			->add_fields( array(
-				Field::make( 'text', 'c_name', 'Наименование параметра' )->set_width(50),
-				Field::make( 'text', 'c_val',  'Значение' )->set_width(50),
-			) ),
+		Field::make('text', 'offer_sku', 'Артикул (Базовый)')->set_width(50),
+		Field::make('text', 'offer_nal', 'Наличие на складе')->set_default_value('В наличии')->set_width(50),
 
-			Field::make('text', 'offer_price', 'Цена (Базовая)')->set_width(50),
-			Field::make('text', 'offer_old_price', 'Старая цена (Базовая)')->set_width(50),
-			
-			Field::make( 'complex', 'offer_modification', "Модификация товара" )
-			->add_fields( array(
-				Field::make('text', 'mod_name', 'Наименование модификации' )->set_width(20),
+		Field::make('complex', 'offer_cherecter', "Характеристики товара")
+			->add_fields(array(
+				Field::make('text', 'c_name', 'Наименование параметра')->set_width(50),
+				Field::make('text', 'c_val',  'Значение')->set_width(50),
+			)),
+
+		Field::make('text', 'offer_price', 'Цена (Базовая)')->set_width(50),
+		Field::make('text', 'offer_old_price', 'Старая цена (Базовая)')->set_width(50),
+
+		Field::make('complex', 'offer_modification', "Модификация товара")
+			->add_fields(array(
+				Field::make('text', 'mod_name', 'Наименование модификации')->set_width(20),
 				Field::make('text', 'mod_sku', 'Артикул модификации')->set_width(20),
 				Field::make('text', 'mod_price', 'Цена модификации')->set_width(20),
 				Field::make('text', 'mod_old_price', 'Старая цена модификации')->set_width(20),
 				Field::make('text', 'mod_picture_id', 'Изображения модификации')->set_width(20),
-			) ),
-			  
-			Field::make( 'complex', 'offer_picture', "Галерея товара" )
-			->add_fields( array(
-				Field::make('image', 'gal_img', 'Изображение' )->set_width(30),
+			)),
+
+		Field::make('complex', 'offer_picture', "Галерея товара")
+			->add_fields(array(
+				Field::make('image', 'gal_img', 'Изображение')->set_width(30),
 				Field::make('text', 'gal_img_sku', 'ID для модификации')->set_width(30),
-				Field::make('text', 'gal_img_alt', 'alt и title')->set_width(30)				
-			) ),
+				Field::make('text', 'gal_img_alt', 'alt и title')->set_width(30)
+			)),
 
-			Field::make('rich_text', 'offer_fulltext', 'Полное описание (SEO)')->set_width(50),
+		Field::make('rich_text', 'offer_fulltext', 'Полное описание (SEO)')->set_width(50),
 
-			Field::make( 'complex', 'offer_rev', "Отзывы о товаре" )
-			->add_fields( array(
-				Field::make('text', 'rev_name', 'Имя' )->set_width(20),
-				Field::make('text', 'rev_mail', 'e-mail' )->set_width(20),
-				Field::make('date', 'rev_date', 'Дата отзыва' )->set_width(20),
-				Field::make('select', 'rev_reiting', 'Оценка' )->add_options( array(
+		Field::make('complex', 'offer_rev', "Отзывы о товаре")
+			->add_fields(array(
+				Field::make('text', 'rev_name', 'Имя')->set_width(20),
+				Field::make('text', 'rev_mail', 'e-mail')->set_width(20),
+				Field::make('date', 'rev_date', 'Дата отзыва')->set_width(20),
+				Field::make('select', 'rev_reiting', 'Оценка')->add_options(array(
 					'1' => '1',
 					'2' => '2',
 					'3' => '3',
 					'4' => '4',
 					'5' => '5'
-				) )->set_width(20),
+				))->set_width(20),
 				Field::make('rich_text', 'rev_text', 'Текст отзыва')->set_width(100),
-				Field::make('rich_text', 'rev_otv', 'Ответ')->set_width(100)				
-			) ),
-			
-			
-			
-		  ));
+				Field::make('rich_text', 'rev_otv', 'Ответ')->set_width(100)
+			)),
 
-		  
-?>
+
+
+	));
