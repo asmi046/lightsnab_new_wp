447ce8fac1659857af154e0b542593a3fbc5c998
Ruvick <rudikov.web@ya.ru>
2022-04-06 10:03:09 +0300

    Стилизовал кнопку

--- template-parts/about-section.php
+++ template-parts/about-section.php
@@ -41,7 +41,7 @@
       <div class="project-block__text project-calc__text">
         <h3 class="project-calc__title">Есть проект на рассчет?</h3>
         <p class="project-calc__subtitle">Отправьте проект и мы постараемся сделать цену ниже.</p>
-        <button type="submit" class="project-calc__btn _popup-link">Отправить проект…</button>
+        <a href="#project" class="project-calc__btn _popup-link">Отправить проект</a>
       </div>
     </div>
 
