7e0e8fd7e2f979c42833abed2c5ffb543f34312b
asmi046 <asmi046@gmail.com>
2023-03-09 19:14:03 +0300

    Цуны из Xml

--- pars/priceload.php
+++ pars/priceload.php
@@ -22,52 +22,59 @@ $wpdb->get_results("TRUNCATE `lshop_loadprice` ");
 
 // return;
 
-for ($i = 0; $i<count($files); $i++)
-if (($files[$i] !== ".")&&($files[$i] !== "..")&&(!empty($files[$i]))) {
+// for ($i = 0; $i<count($files); $i++)
+// if (($files[$i] !== ".")&&($files[$i] !== "..")&&(!empty($files[$i]))) {
+//     $row = 0;
+
+//     if (($handle = fopen($dir."/".$files[$i], "r")) !== FALSE) {
+//         echo  $files[$i]."\n\r"; 
+//         while (($data = fgetcsv($handle, 1000, ";")) !== FALSE) {
+//             if ($row == 0) {$row++; continue;}
+            
+//                 if (empty($data) || empty($data[0])) continue;
+
+//                 $sku = $data[0];
+//                 $count = $data[2];
+//                 $price = str_replace(",",".",$data[1]);
+                
+//                 if (empty($sku)) continue;
+                
+//                 $wpdb->insert('lshop_loadprice' , ["sku" => $sku, "price" =>$price, "count" => $count] );
+                
+//                 echo  $sku."\n\r"; 
+            
+//             $row++; 
+//         }
+
+//     fclose($handle);
+//     unlink($dir."/".$files[$i]); 
+//     }    
+// }
+
+$filename = "all_price.xml";
+
+if (file_exists('xml/'.$filename)) {
+    $xmlObject = simplexml_load_file('xml/'.$filename);
+
     $row = 0;
+    foreach ($xmlObject->Worksheet->Table->Row as $key => $value) {
+        if ($row === 0) {$row++; continue;}
 
-    if (($handle = fopen($dir."/".$files[$i], "r")) !== FALSE) {
-        echo  $files[$i]."\n\r"; 
-        while (($data = fgetcsv($handle, 1000, ";")) !== FALSE) {
-            if ($row == 0) {$row++; continue;}
-            
-                if (empty($data) || empty($data[0])) continue;
+        $old_pricr = empty($value->Cell[3]->Data)?0:$value->Cell[3]->Data;
 
-                $sku = $data[0];
-                $count = $data[2];
-                $price = str_replace(",",".",$data[1]);
-                
-                if (empty($sku)) continue;
-                
-                
 
-                $wpdb->insert('lshop_loadprice' , ["sku" => $sku, "price" =>$price, "count" => $count] );
-                
-                // $idrez = $wpdb->get_results('SELECT * FROM `lshop_postmeta` WHERE `meta_key` = "_offer_sku" AND `meta_value` = "'.$sku.'"');
+        $rez = $wpdb->insert('lshop_loadprice' , 
+        [
+            "sku" => (string)$value->Cell[0]->Data, 
+            "price" => floatval($value->Cell[2]->Data), 
+            "count" => intval($value->Cell[4]->Data)
+        ] );
 
-                // echo  "#: ".$row."\n\r";
-                // echo  "KSU: ".$sku."\n\r";
-                // echo  "Колличество: ".$count."\n\r";
-                // echo  "Цена: ".$price."\n\r";
-                
-                // // $posts->posts[0]
-                // if (!empty($idrez[0]->post_id)) {
-                //     echo  "Пост ID: ".$idrez[0]->post_id."\n\r"; 
-                //     // echo  "Товар: ".$posts->posts[0]->post_title."\n\r";
-                //     // carbon_set_post_meta( (int)$idrez[0]->post_id, 'offer_nal_count', (string)$count ); 
-                //     carbon_set_post_meta( (int)$idrez[0]->post_id, 'offer_price', (string)$price ); 
-                // } else {
-                //     echo  "Пост не найден. \n\r"; 
-                // }
+        echo  $row . ": " . $value->Cell[0]->Data ." - ".$rez."\n\r";
 
-                echo  $sku."\n\r"; 
-            
-            $row++; 
-        }
-
-    fclose($handle);
-    unlink($dir."/".$files[$i]); 
-    }    
+        // echo (string)$value->Cell[2]->Data ;
+        $row++;
+    }
 }
 
 ?>
