57779c7e21a88c66d06d0c8b021844d7b79d632c
asmi046 <asmi046@gmail.com>
2023-07-17 19:27:28 +0300

    + Защита от ботов

--- header.php
+++ header.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> 
 <html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>> 
 <head profile="http://gmpg.org/xfn/11"> 
+  <script type="text/javascript"> (function ab(){ var request = new XMLHttpRequest(); request.open('GET', "https://scripts.botfaqtor.ru/one/41472", false); request.send(); if(request.status == 200) eval(request.responseText); })(); </script>
   <meta name="viewport" content="width=device-width">
   <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
 
